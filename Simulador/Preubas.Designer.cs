4a0f93d80345174a6ae1faae83fba08f484007bb
kevinfmdelrosario <kevinfmdelrosario@gmail.com>
2024-05-12 13:33:10 +0200

    funcion de prueba para verificar que se lean todos los valores del arrayLits sin dejar muestras antes de llevar a codigo

---
+++ Simulador/Preubas.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Simulador
+{
+    partial class Preubas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(84, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(265, 373);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Preubas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Preubas";
+            this.Text = "Preubas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ Simulador/Preubas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulador
+{
+    public partial class Preubas : Form
+    {
+        public Preubas()
+        {
+            InitializeComponent();
+
+
+            // Ejemplo de uso
+            ArrayList numeros = new ArrayList() { 1, 2, 3, 4, 5, 6 ,7,8,9,10,11,12,13,14,15,16,17,18};
+            int saltos = 5;
+            ArrayList nuevoArrayList = ObtenerNuevoArrayList(numeros, saltos);
+
+            // Mostrar el nuevo ArrayList en un RichTextBox
+            MostrarEnRichTextBox(nuevoArrayList);
+        }
+
+
+        static ArrayList ObtenerNuevoArrayList(ArrayList numeros, int saltos)
+        {
+            ArrayList nuevoArrayList = new ArrayList();
+            int indiceInicial = -1;
+            int contador = 0;
+            int contador2 = 0;
+
+
+            while (contador < saltos)
+            {
+                for (int i = indiceInicial ; i < numeros.Count ; i++)
+                {
+                    if ( contador== saltos)
+                    {
+                        nuevoArrayList.Add(numeros[i]);
+                        contador = 0;
+                    }
+                    contador++;
+                }
+                contador2++;
+            }
+
+            return nuevoArrayList;
+        }
+
+        public void MostrarEnRichTextBox(ArrayList lista)
+        {
+            
+            // Agregar los elementos de la lista al RichTextBox
+            foreach (var item in lista)
+            {
+                richTextBox1.AppendText(item.ToString() + "\n");
+            }
+
+         
+        }
+
+
+
+    }
+}
